8a5298d325e1980c91a88bf7428fa532d844fb12
Kelvin <kelvs.r@icloud.com>
2024-05-01 13:54:24 +0800

    Initial Commit

---
+++ TicTacToeClassic/TicTacToeClassicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeClassicApp.swift
+//  TicTacToeClassic
+//
+//  Created by Kelvin on 01/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeClassicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

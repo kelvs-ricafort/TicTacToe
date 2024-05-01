488c16bc1f4f7a9abd17dd846aab6a8c6e1f80e6
Kelvin <kelvs.r@icloud.com>
2024-05-01 18:16:07 +0800

    - GameService Class - Creating Game Board - Custom Button Style - Two Player Game Code

--- TicTacToeClassic/StartView.swift
+++ TicTacToeClassic/StartView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct StartView: View {
+    @EnvironmentObject var game: GameService
     @State private var gameType: GameType = .undetermined
     @State private var yourName = ""
     @State private var opponentName = ""
@@ -46,6 +47,7 @@ struct StartView: View {
             .frame(width: 350)
             if gameType != .peer {
                 Button("Start Game") {
+                    game.setupGame(gameType: gameType, player1Name: yourName, player2Name: opponentName)
                     focus = false
                     startGame.toggle()
                 }
@@ -72,5 +74,6 @@ struct StartView: View {
 struct StartView_Previews: PreviewProvider {
     static var previews: some View {
         StartView()
+            .environmentObject(GameService())
     }
 }
